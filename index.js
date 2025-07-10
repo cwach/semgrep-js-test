eff981b9e2583b54b68b777a77a10008449e61b7
cwach <wchchrs@gmail.com>
2025-07-10 14:08:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 alert('Hello world');
+eval("2 + 2");           // sollte als Sicherheitsproblem erkannt werden
+document.write("Hello"); // auch kritisch
+alert("This is unsafe"); // manchmal erkannt, je nach Regel
